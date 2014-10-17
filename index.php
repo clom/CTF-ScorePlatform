aa10cb5961806324a852d3b86c688673d4bb4b88
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2014-10-17 19:10:37 +0900

    各 php ファイルの共通部分を，ひとまとめにした
    navbar に class="active" の記述があるので，なんとかする

--- index.php
+++ index.php
@@ -1,62 +1,8 @@
 <?php
 	session_start();
 	require_once(dirname(__FILE__) . "/conf.php");
+  include 'header.html';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<!-- Note there is no responsive meta tag here -->
-
-	<link rel="shortcut icon" href="./assets/ico/favicon.png">
-
-	<title>Project misaka</title>
-
-	<!-- Bootstrap core CSS -->
-	<link href="./dist/css/bootstrap.css" rel="stylesheet">
-
-	<!-- Custom styles for this template -->
-	<link href="non-responsive.css" rel="stylesheet">
-
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="./assets/js/html5shiv.js"></script>
-	<script src="./assets/js/respond.min.js"></script>
-	<![endif]-->
-
-	</head>
-
-	<body>
-	<!-- Fixed navbar -->
-	<div class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="./">CTF Score Platform</a>
-			</div>
-			<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="./">Home</a></li>
-					<li><a href="./notice.php">Notice</a></li>
-					<?php
-					if($_SESSION['flag']){
-						echo'
-					<li><a href="./challenge.php">Challenge</a></li>
-					<li><a href="./rank.php">Ranking</a></li>
-					';
-					}
-					?>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</div>
 
 	<div class="container">
 
--- read.php
+++ read.php
@@ -2,65 +2,6 @@
 	session_start();
 	require_once(dirname(__FILE__) . "/conf.php");
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<!-- Note there is no responsive meta tag here -->
-
-	<link rel="shortcut icon" href="./assets/ico/favicon.png">
-
-	<title>Project misaka</title>
-
-	<!-- Bootstrap core CSS -->
-	<link href="./dist/css/bootstrap.css" rel="stylesheet">
-
-	<!-- Custom styles for this template -->
-	<link href="non-responsive.css" rel="stylesheet">
-
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-	<script src="./assets/js/html5shiv.js"></script>
-	<script src="./assets/js/respond.min.js"></script>
-	<![endif]-->
-
-	</head>
-
-	<body>
-	<!-- Fixed navbar -->
-	<div class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="./">CTF Score Platform</a>
-			</div>
-			<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="./">Home</a></li>
-					<li class="active"><a href="./notice.php">Notice</a></li>
-					<?php
-					if($_SESSION['flag']){
-						echo'
-					<li><a href="./challenge.php">Challenge</a></li>
-					<li><a href="./rank.php">Ranking</a></li>
-					';
-					}
-					?>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</div>
-
-	<?php
-	
 	$title = $_GET['title'];
 
 	$con = mysql_connect($db_url, $db_user, $db_pass);
@@ -68,6 +9,8 @@
 	$result = mysql_query('SET NAMES utf8', $con);
 	$result = mysql_query("SELECT * FROM notice WHERE title='".$title."'", $con);
 	$rows = mysql_num_rows($result);
+
+  include 'header.html';
 	?>
 
 	<div class="container">
@@ -83,15 +26,13 @@
 			echo $value;
 		}
 	}
+
+  $con = mysql_close($con);
 	?>
 
-	<?php
-		$con = mysql_close($con);
-	?>
-
-	<footer>
-		Copyright (C) 2014 clom-networks. All Rights Reserved.
-	</footer>
+	  <footer>
+	  	Copyright (C) 2014 clom-networks. All Rights Reserved.
+	  </footer>
 
 	</div> <!-- /container -->
 
